21b180964a8f6cd853dd5d346183305b4cf4b985
Ahmed Arslan <ahmedakhokhar@gmail.com>
2024-04-12 06:46:56 -0700

    Update stepOne.R

--- stepOne.R
+++ stepOne.R
@@ -9,9 +9,9 @@ SE <- TwoSampleMR:::get_se(exposures$BETA, exposures$P)
 exposures$se <- SE
 colnames(exposures) <- c("SNP", "chr", "position", "effect_allele", "other_allele", "beta", "Phenotype", "pval", "se", "eaf", "samplesize")
 exposures_frmt <- format_data(exposures, type = "exposure")
-exposures_clump_local <- ld_clump(dplyr::tibble(rsid=exposures$SNP, pval=exposures$pval, id=exposures$Phenotype), plink_bin = genetics.binaRies::get_plink_binary(), bfile = "/path/to/LD/EUR")
+exposures_clump_local_preH <- ld_clump(dplyr::tibble(rsid=exposures$SNP, pval=exposures$pval, id=exposures$Phenotype), plink_bin = genetics.binaRies::get_plink_binary(), bfile = "/path/to/LD/EUR")
 exposures_clump_local_preH$pval <- NULL
 exposures_clump_local_preH$id <- NULL
-rm(exposures_clump_local)
+#rm(exposures_clump_local)
 #keep count of exposures snps
 exposures_snps_count <- exposures_clump_local_preH %>% count(exposure); exposures_snps_count <- arrange(exposures_snps_count, desc(n))
